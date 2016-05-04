a777abfaf6a3d5ba3eb801f4cbea90065776023a
Brandon Wood <bcwood@uaa.alaska.edu>
2016-05-04 15:05:46 -0800

    added author display name

--- PressSharp/Author.cs
+++ PressSharp/Author.cs
@@ -1,10 +1,10 @@
-﻿
-namespace PressSharp
+﻿namespace PressSharp
 {
     public class Author
     {
         public string Id { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
+        public string DisplayName { get; set; }
     }
 }
